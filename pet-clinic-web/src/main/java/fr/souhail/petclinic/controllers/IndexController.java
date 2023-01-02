ab26f41b212a250f783f47ba274a1302719e8ae1
souhail-kaci <souhail_196@hotmail.com>
2023-01-02 22:10:34 +0100

    import master layout in views

--- pet-clinic-web/src/main/java/fr/souhail/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/fr/souhail/petclinic/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package fr.souhail.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("welcome", "Welcome");
         return "index";
     }
 
